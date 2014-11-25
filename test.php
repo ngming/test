5df463f28e00f1b8d6ea7ac4adc9cd2ea5a97d21
ngming <intdev5@jasoncs-iMac.local>
2014-11-25 17:52:58 -0500

    this

--- test.php
+++ test.php
@@ -7,6 +7,7 @@
 <body>
 
 <header>ming</header>
+<div>
     <div>testing</div>
     <div><li>lilili</li></div>
     <div>hahahahah</div>
@@ -15,6 +16,7 @@
     <div>rebase1</div>
     <div>rebase2</div>
     <div>rebase3</div>
+</div>
 <footer>ming footer</footer>
 
     </body>
