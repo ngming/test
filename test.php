12dfe2c1452782041b94d8725426f0269d9f156b
ngming <intdev5@jasoncs-iMac.local>
2014-11-25 17:54:21 -0500

    hahah

--- test.php
+++ test.php
@@ -8,11 +8,6 @@
 
 <header>ming</header>
 <div>
-    <div>testing</div>
-    <div><li>lilili</li></div>
-    <div>hahahahah</div>
-    <div>add 1</div>
-    <div>add 2</div>
     <div>rebase1</div>
     <div>rebase2</div>
     <div>rebase3</div>
